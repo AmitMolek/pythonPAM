2e9a0309a20b80395a10b9b0ed5195f40d8c72d2
Amit Molek <iamitmolek@gmail.com>
2020-05-25 17:39:01 +0300

    Added code to write results to excel

--- pythonPAM.py
+++ pythonPAM.py
@@ -6,6 +6,7 @@ from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.cluster import cluster_visualizer
 from pyclustering.utils import read_sample
 from pyclustering.samples.definitions import FCPS_SAMPLES
+from xlwt import Workbook
 
 def calculate_distance_matrix(dataset):
     dist_arr = pdist(dataset)
@@ -74,48 +75,128 @@ def getRandomDataset(size):
     return arr.T
 
 
+def runPam():
+    #dataset = np.array([[1,1], [2,1], [0,1], [1,2], [1,0], [5,5], [6,5], [4,5], [5,6], [5,4]], dtype=float)
+    #dataset = getRandomDataset(1000)
+
+    sample = read_sample(FCPS_SAMPLES.SAMPLE_TWO_DIAMONDS)
+    trimmed_sample = sample[:100]
+    dataset = np.asarray(trimmed_sample, dtype=float)
+
+    #distance_matrix = calculate_distance_matrix(dataset)
+    #calculateTotalCost(dataset, np.array([[1], [2]]), distance_matrix)
+
+    print("Dataset size =", len(trimmed_sample))
+
+    for k in range(1,11):
+        print("----- k =", k, "-----")
+
+        pam_start_time = time.time()
+
+        medoids, config, cost = pam(dataset, k)
+
+        pam_end_time = time.time()
+
+        elapsedRunTime = (pam_end_time - pam_start_time) * 1000
+
+        #print("Config Cost =", cost)
+        #print("Medoids:")
+        #print(dataset[medoids])
+        #print("Dataset Labeling: ")
+        #print(np.concatenate((dataset, dataset[medoids[config]]), axis=1))
+
+        print("Our Pam run for {:.3f}".format(elapsedRunTime))
+        print()
+
+        pycluster_start_time = time.time()
+        initial_medoids = select_randomly(np.asarray(trimmed_sample, dtype=float), k)
+
+        kmedoids_instance = kmedoids(sample, initial_medoids, ccore=False)
+
+        kmedoids_instance.process()
+
+        pycluster_end_time = time.time()
+
+        elapsedPyclusterRunTime = (pycluster_end_time - pycluster_start_time) * 1000
+
+        print("Pycluster's Pam (CCore=False) run for {:.3f}".format(elapsedPyclusterRunTime))
+
+        pycluster_start_time_ccore_on = time.time()
+
+        kmedoids_instance = kmedoids(sample, initial_medoids, ccore=True)
+
+        kmedoids_instance.process()
+
+        pycluster_end_time_ccore_on = time.time()
+
+        elapsedPyclusterCCoreRunTime = (pycluster_end_time_ccore_on - pycluster_start_time_ccore_on) * 1000
+
+        print("Pycluster's Pam (CCore=True) run for {:.3f}".format(elapsedPyclusterCCoreRunTime))
+
+
+def runPamExcel():
+    wb = Workbook()
+
+    excelSheet = wb.add_sheet('Sheet_1')
+
+    sample = read_sample(FCPS_SAMPLES.SAMPLE_TWO_DIAMONDS)
+    trimmed_sample = sample[:400]
+    dataset = np.asarray(trimmed_sample, dtype=float)
+
+    excelSheet.write(0, 0, 'Dataset Size=')
+    excelSheet.write(0, 1, len(trimmed_sample))
+
+    min_k = 1
+    max_k = 10
+
+    run_t_times = 3
+
+    start_line = 3
+
+    excelSheet.write(start_line - 1, 0, 'k')
+
+    for k in range(min_k, max_k + 1):
+        excelSheet.write(start_line - 1, k, k)
+
+    for i in range(run_t_times):
+        excelSheet.write(start_line + i, 0, 'Our')
+
+        for k in range(min_k, max_k + 1):
+            pam_start_time = time.time()
+            medoids, config, cost = pam(dataset, k)
+            pam_end_time = time.time()
+            elapsedRunTime = (pam_end_time - pam_start_time) * 1000
+
+            excelSheet.write(start_line + i, k, elapsedRunTime)
+
+    for i in range(run_t_times):
+        excelSheet.write(15 + start_line + i, 0, 'Pycluster CCORE off')
+
+        for k in range(min_k, max_k + 1):
+            pycluster_start_time = time.time()
+            initial_medoids = select_randomly(np.asarray(trimmed_sample, dtype=float), k)
+            kmedoids_instance = kmedoids(sample, initial_medoids, ccore=False)
+            kmedoids_instance.process()
+            pycluster_end_time = time.time()
+            elapsedPyclusterRunTime = (pycluster_end_time - pycluster_start_time) * 1000
+
+            excelSheet.write(15 + start_line + i, k, elapsedPyclusterRunTime)
+
+    for i in range(run_t_times):
+        excelSheet.write(30 + start_line + i, 0, 'Pycluster CCORE on')
+
+        for k in range(min_k, max_k + 1):
+            pycluster_start_time_ccore_on = time.time()
+            kmedoids_instance = kmedoids(sample, initial_medoids, ccore=True)
+            kmedoids_instance.process()
+            pycluster_end_time_ccore_on = time.time()
+            elapsedPyclusterCCoreRunTime = (pycluster_end_time_ccore_on - pycluster_start_time_ccore_on) * 1000
+
+            excelSheet.write(30 + start_line + i, k, elapsedPyclusterCCoreRunTime)
+
+    wb.save('_example.xls') 
+
+
 np.set_printoptions(precision=2)
-#dataset = np.array([[1,1], [2,1], [0,1], [1,2], [1,0], [5,5], [6,5], [4,5], [5,6], [5,4]], dtype=float)
-#dataset = getRandomDataset(1000)
-
-sample = read_sample(FCPS_SAMPLES.SAMPLE_TWO_DIAMONDS)
-trimmed_sample = sample[:100]
-dataset = np.asarray(trimmed_sample, dtype=float)
-
-#distance_matrix = calculate_distance_matrix(dataset)
-#calculateTotalCost(dataset, np.array([[1], [2]]), distance_matrix)
-
-print("Dataset size =", len(trimmed_sample))
-
-for k in range(1,11):
-    print("----- k =", k, "-----")
-
-    pam_start_time = time.time()
-
-    medoids, config, cost = pam(dataset, k)
-
-    pam_end_time = time.time()
-
-    elapsedRunTime = (pam_end_time - pam_start_time) * 1000
-
-    #print("Config Cost =", cost)
-    #print("Medoids:")
-    #print(dataset[medoids])
-    #print("Dataset Labeling: ")
-    #print(np.concatenate((dataset, dataset[medoids[config]]), axis=1))
-
-    print("Our Pam run for {:.3f}".format(elapsedRunTime))
-    print()
-
-    pycluster_start_time = time.time()
-    initial_medoids = select_randomly(np.asarray(trimmed_sample, dtype=float), k)
-
-    kmedoids_instance = kmedoids(sample, initial_medoids)
-
-    kmedoids_instance.process()
-
-    pycluster_end_time = time.time()
-
-    elapsedPyclusterRunTime = (pycluster_end_time - pycluster_start_time) * 1000
-
-    print("Pycluster's Pam run for {:.3f}".format(elapsedPyclusterRunTime))
+#runPamExcel()
+runPam()
